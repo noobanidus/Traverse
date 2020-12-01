ed4d9a3cd187f943ba7a52ad8a74a82471dabe38
Jon McManus <due@wxwhatever.com>
2020-12-01 13:36:06 +1000

    Clean all of this up.

--- src/main/java/com/terraformersmc/traverse/generator/GenDataCommand.java
+++ src/main/java/com/terraformersmc/traverse/generator/GenDataCommand.java
@@ -1,16 +1,12 @@
 package com.terraformersmc.traverse.generator;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
+import com.google.gson.*;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.CommandManager;
@@ -19,22 +15,20 @@ import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
-import net.minecraft.util.registry.MutableRegistry;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeEffects;
-import net.minecraft.world.biome.GenerationSettings;
-import net.minecraft.world.biome.SpawnSettings;
-import net.minecraft.world.gen.carver.Carver;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.carver.ConfiguredCarver;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
-import net.minecraft.world.gen.feature.ConfiguredStructureFeatures;
 import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
-import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -60,34 +54,17 @@ public class GenDataCommand {
 		dispatcher.register(CommandManager.literal(commandString).redirect(source));
 	}
 
-  public static final Codec<Biome> CODEC = RecordCodecBuilder.create((instance) -> {
-    return instance.group(Biome.Weather.CODEC.forGetter((biome) -> {
-      return biome.weather;
-    }), Biome.Category.CODEC.fieldOf("category").forGetter((biome) -> {
-      return biome.category;
-    }), Codec.FLOAT.fieldOf("depth").forGetter((biome) -> {
-      return biome.depth;
-    }), Codec.FLOAT.fieldOf("scale").forGetter((biome) -> {
-      return biome.scale;
-    }), BiomeEffects.CODEC.fieldOf("effects").forGetter((biome) -> {
-      return biome.effects;
-    }), GenerationSettings.CODEC.forGetter((biome) -> {
-      return biome.generationSettings;
-    }), SpawnSettings.CODEC.forGetter((biome) -> {
-      return biome.spawnSettings;
-    })).apply(instance, Biome::new);
-  });
-
 	public static void createBiomeDatapack(String modId, CommandContext<ServerCommandSource> commandSource) {
 		List<Biome> biomeList = new ArrayList<>();
 		boolean stopSpamFlag = false;
 		Path dataPackPath = dataPackPath(commandSource.getSource().getWorld().getServer().getSavePath(WorldSavePath.DATAPACKS), modId);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		Registry<Biome> biomeRegistry = commandSource.getSource().getMinecraftServer().getRegistryManager().get(Registry.BIOME_KEY);
+		DynamicRegistryManager manager = commandSource.getSource().getMinecraftServer().getRegistryManager();
+		Registry<Biome> biomeRegistry = manager.get(Registry.BIOME_KEY);
+		Registry<ConfiguredFeature<?, ?>> featuresRegistry = manager.get(Registry.CONFIGURED_FEATURE_WORLDGEN);
+		Registry<ConfiguredStructureFeature<?, ?>> structuresRegistry = manager.get(Registry.CONFIGURED_STRUCTURE_FEATURE_WORLDGEN);
+		Registry<ConfiguredCarver<?>> carverRegistry = manager.get(Registry.CONFIGURED_CARVER_WORLDGEN);
 
-
-
-		//Collect biomes from the datapack registry where biome data is most likely to have been modified by other content adding mods.
 		for (Map.Entry<RegistryKey<Biome>, Biome> biome : biomeRegistry.getEntries()) {
 			if (Objects.requireNonNull(biomeRegistry.getKey(biome.getValue())).toString().contains(modId)) {
 				biomeList.add(biome.getValue());
@@ -105,7 +82,36 @@ public class GenDataCommand {
 							Files.createDirectories(biomeJsonPath.getParent());
 							Optional<JsonElement> optional = (biomeCodec.apply(() -> biome).result());
 							if (optional.isPresent()) {
-								Files.write(biomeJsonPath, gson.toJson(optional.get()).getBytes());
+								JsonElement root = optional.get();
+								JsonArray features = new JsonArray();
+								for (List<Supplier<ConfiguredFeature<?, ?>>> list : biome.getGenerationSettings().getFeatures()) {
+									JsonArray stage = new JsonArray();
+									for (Supplier<ConfiguredFeature<?, ?>> feature : list) {
+										featuresRegistry.getKey(feature.get()).ifPresent(k -> stage.add(k.getValue().toString()));
+									}
+									features.add(stage);
+								}
+								root.getAsJsonObject().add("features", features);
+								String surfaceBuilder = root.getAsJsonObject().get("surface_builder").getAsJsonObject().get("type").getAsString();
+								root.getAsJsonObject().addProperty("surface_builder", surfaceBuilder);
+
+								JsonObject carvers = new JsonObject();
+								for (GenerationStep.Carver step : GenerationStep.Carver.values()) {
+									JsonArray stage = new JsonArray();
+									for (Supplier<ConfiguredCarver<?>> carver : biome.getGenerationSettings().getCarversForStep(step)) {
+										carverRegistry.getKey(carver.get()).ifPresent(k -> stage.add(k.getValue().toString()));
+									}
+									if (stage.size() > 0) {
+										carvers.add(step.asString(), stage);
+									}
+								}
+								root.getAsJsonObject().add("carvers", carvers);
+								JsonArray starts = new JsonArray();
+								for (Supplier<ConfiguredStructureFeature<?, ?>> start : biome.getGenerationSettings().getStructureFeatures()) {
+									structuresRegistry.getKey(start.get()).ifPresent(k -> starts.add(k.getValue().toString()));
+								}
+								root.getAsJsonObject().add("starts", starts);
+								Files.write(biomeJsonPath, gson.toJson(root).getBytes());
 							}
 						}
 					} catch (IOException e) {
@@ -129,10 +135,6 @@ public class GenDataCommand {
 		} else {
 			commandSource.getSource().sendFeedback(new TranslatableText("commands.gendata.listisempty", modId).styled(text -> text.withColor(TextColor.fromFormatting(Formatting.RED))), false);
 		}
-	}
-
-	private static Path object(Path path, Identifier identifier, String modId, String type) {
-		return path.resolve("data/" + modId + "/worldgen/dump/" + type + "/" + identifier.getNamespace() + "." + identifier.getPath() + ".json");
 	}
 
 	private static Path jsonPath(Path path, Identifier identifier, String modId) {
